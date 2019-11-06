077e40f0494efcbae696de957169072b6c08210b
adeoye adela <adeoye.adela@gmail.com>
2019-11-06 23:57:01 +0100

    Meet MVP and work on stretch goal to make use of hooks instead of HOC

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import AddedFeature from './AddedFeature';
-import { connect } from 'react-redux';
- 
+// import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
+
 const AddedFeatures = props => {
+  const car= useSelector(state=>state.car)
   return (
     <div className="content">
       <h6>Added features:</h6>
-      {props.car.features.length ? (
+      {car.features.length ? (
         <ol type="1">
-          {props.car.features.map(item => (
+          {car.features.map(item => (
             <AddedFeature key={item.id} removeFeatures={props.removeFeatures} feature={item} />
           ))}
         </ol>
@@ -20,9 +22,9 @@ const AddedFeatures = props => {
   );
 };
 
-const mapStateToProps= state=>{
-  return {
-    car: state.car
-  }
-}
-export default connect(mapStateToProps,{})(AddedFeatures);
+// const mapStateToProps= state=>{
+//   return {
+//     car: state.car
+//   }
+// }
+export default AddedFeatures;
--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import AdditionalFeature from './AdditionalFeature';
 import { connect } from 'react-redux';
-
+import {useSelector} from 'react-redux';
 const AdditionalFeatures = props => {
-
+const additionalFeatures= useSelector(state=>state.additionalFeatures)
   return (
     <div className="content">
       <h4>Additional Features</h4>
-      {props.additionalFeatures.length ? (
+      {additionalFeatures.length ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
+          {additionalFeatures.map(item => (
             <AdditionalFeature key={item.id} buyItems={props.buyItems} feature={item} />
           ))}
         </ol>
@@ -20,9 +20,9 @@ const AdditionalFeatures = props => {
   );
 };
 
-const mapStateToProps= state=>{
-  return {
-    additionalFeatures:state.additionalFeatures,
-  }
-}
-export default connect(mapStateToProps,{})(AdditionalFeatures);
+// const mapStateToProps= state=>{
+//   return {
+//     additionalFeatures:state.additionalFeatures,
+//   }
+// }
+export default AdditionalFeatures;
